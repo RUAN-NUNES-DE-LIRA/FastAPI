b591b839675bd0714e56792729ede8b6a62b6c99
RuandersonNunes <ruanderson956@gmail.com>
2025-08-15 04:05:53 -0300

    finalized crud

--- FastAPI-basic/main.py
+++ FastAPI-basic/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
-from models import Curso
-from fastapi import FastAPI, HTTPException, status
+from models import Course
+from fastapi import FastAPI, HTTPException, status, Response
 
 app = FastAPI()
 
@@ -50,13 +50,32 @@ async def get_course(course_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found a course with this ID')
 
 @app.post('/courses', status_code=status.HTTP_201_CREATED)
-async def post_course(course: Curso):
+async def post_course(course: Course):
     next_id = len(courses) + 1
     courses[next_id] = course
 
     del course.id
     return course
 
+
+@app.put("/courses/{course_id}")
+async def put_course(course_id, course: Course):
+    if course_id in courses:
+        courses[course_id] = course
+
+        del course.id
+        return course
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not Found a course with this ID: {course_id}")
+    
+@app.delete("/courses/{course_id}")
+async def delete_course(course_id: int):
+    if course_id in courses:
+        del courses[course_id]
+
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not Found a course with this ID: {course_id}")
 
 if __name__ == '__main__':
 
--- FastAPI-basic/models.py
+++ FastAPI-basic/models.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 
-class Curso(BaseModel):
+class Course(BaseModel):
 
     id: Optional[int] = None
     title: str
